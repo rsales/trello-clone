0b9119417e2eb21c72519a6b03d18f7b289c9777
Rafael Sales <rafael.sales93@hotmail.com>
2020-04-13 23:45:25 -0300

    Lesson 2: Building our Board

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="board">
-
+    <div class="flex flex-row items-start">
+      <div
+        v-for="(column, $columnIndex) of board.columns"
+        :key="$columnIndex"
+        class="column"
+      >
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div
+            class="task"
+            v-for="(task, $taskIndex) of column.tasks"
+            :key="$taskIndex"
+          >
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              v-if="task.descripion"
+              class="w-full flex-no-shrink mt-1 tx-sm"
+            >
+              {{ task.descripion }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  computed: mapState(['board'])
 }
 </script>
 
